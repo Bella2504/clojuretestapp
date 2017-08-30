cbd97bae51a6541dc44c00134fc99ba4ab35c74d
Bella2504 <kepka.iza@gmail.com>
2017-08-30 22:30:56 +0200

    initial commit

---
+++ test/testclojureapp/core_test.clj
@@ -0,0 +1,7 @@
+(ns testclojureapp.core-test
+  (:require [clojure.test :refer :all]
+            [testclojureapp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
